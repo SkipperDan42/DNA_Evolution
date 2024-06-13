1fe532147d45b4a56c29467035a92786c4e59c4f
SkipperDan42 <northdan51@gmail.com>
2024-06-13 13:48:01 +0100

    Transcription Errors - IN PROGRESS
    Transcription Errors now work for Random Mutations only.

--- gene_dictionaries.py
+++ gene_dictionaries.py
@@ -1,44 +1,72 @@
 import dna_dictionaries as dna
 
-hairGenes = {"Hair Texture": dna.hair_textures,
-             "Hair Type":    dna.hair_types,
-             "Hair Colour":  dna.hair_colours}
 
-eyeGenes = {"Eye Tone":      dna.eye_tones,
-            "Eye Primary Colour":   dna.eye_primary_colours,
-            "Eye Secondary Colour": dna.eye_secondary_colours}
 
 skinGenes = {"Skin Type":   dna.skin_types,
              "Skin Tone":   dna.skin_tones,
              "Skin Colour": dna.skin_colours}
 
-bodyGenes = {"Body Type":   dna.body_types,
-             "Neck Type":   dna.neck_types,
-             "Shoulder Type": dna.shoulder_types,
-             "Chest Shape": dna.chest_shapes,
-             "Waist Type": dna.waist_types,
-             "Hip Type": dna.hip_types,
-             "Heights":   dna.body_types}
+bodyGenes = {"Body Type":       dna.body_types,
+             "Neck Type":       dna.neck_types,
+             "Shoulder Type":   dna.shoulder_types,
+             "Chest Shape":     dna.chest_shapes,
+             "Waist Type":      dna.waist_types,
+             "Hip Type":        dna.hip_types,
+             "Heights":         dna.body_types}
 
-facialGenes = {  "Facial Shape":   dna.facial_shapes,
-                 "Forehead Shape":   dna.forehead_shapes,
-                 "Eyebrow Shape": dna.eyebrow_shapes,
-                 "Nose Shape": dna.nose_shapes,
-                 "Cheeckbone Shape": dna.cheekbone_shapes,
-                 "Chin Type": dna.chin_types}
+hairGenes = {"Hair Texture": dna.hair_textures,
+             "Hair Type":    dna.hair_types,
+             "Hair Colour":  dna.hair_colours}
 
 bodyHairGenes = {"Hairline Type":   dna.hairline_types,
-                 "Eyebrow Type":   dna.eyebrow_types,
-                 "Eyelash Type": dna.eyelash_types,
-                 "Body Hair Type": dna.body_hair_types}
+                 "Eyebrow Type":    dna.eyebrow_types,
+                 "Eyelash Type":    dna.eyelash_types,
+                 "Body Hair Type":  dna.body_hair_types}
 
-eyeShapeGenes = {"Eye Shape":   dna.eye_shapes,
-                 "Eyelid Shape":   dna.eyelid_shapes}
+facialGenes = {  "Facial Shape":        dna.facial_shapes,
+                 "Forehead Shape":      dna.forehead_shapes,
+                 "Eyebrow Shape":       dna.eyebrow_shapes,
+                 "Nose Shape":          dna.nose_shapes,
+                 "Cheeckbone Shape":    dna.cheekbone_shapes,
+                 "Chin Type":           dna.chin_types}
+
+eyeGenes = {"Eye Tone":             dna.eye_tones,
+            "Eye Primary Colour":   dna.eye_primary_colours,
+            "Eye Secondary Colour": dna.eye_secondary_colours}
+
+eyeShapeGenes = {"Eye Shape":       dna.eye_shapes,
+                 "Eyelid Shape":    dna.eyelid_shapes}
 
 lipGenes = {"Smile Type":   dna.smile_types,
-            "Lip Shape":   dna.lip_shapes}
+            "Lip Shape":    dna.lip_shapes}
 
-teethGenes = {"Teeth Type":   dna.teeth_types}
+teethGenes = {"Teeth Type": dna.teeth_types}
+
+earGenes = {"Ear Size":         dna.ear_sizes,
+            "Ear Shape":        dna.ear_shapes,
+            "Earlobe Shape":    dna.earlobe_shapes}
+
+armGenes = {"Upper Arm Size":   dna.upper_arm_sizes,
+            "Forearm Size":     dna.forearm_sizes,
+            "Wrist Size":       dna.wrist_sizes}
+
+handGenes = {"Hand Size":       dna.hand_sizes,
+             "Hand Shape":      dna.hand_shapes,
+             "Finger Length":   dna.finger_lengths,
+             "Nail Shape":      dna.nail_shapes}
+
+legGenes = {"Leg Shape":    dna.leg_shapes,
+            "Thigh Size":   dna.thigh_sizes,
+            "Calf Size":    dna.calf_sizes}
+
+footGenes = {"Ankle Size":  dna.ankle_sizes,
+             "Foot Size":   dna.foot_sizes}
+
+skinPatternGenes = {"Freckle Pattern":      dna.freckle_patterns,
+                    "Mole Pattern":         dna.mole_patterns,
+                    "Birthmark Pattern":    dna.birthmark_patterns}
+
+voiceGene = {"Voice": dna.voice_tones}
 
 
 
@@ -47,8 +75,15 @@ allGenes = {
             "Body": bodyGenes,
             "Hair": hairGenes,
             "Body Hair": bodyHairGenes,
+            "Face": facialGenes,
             "Eyes":  eyeGenes,
             "Eye Shape": eyeShapeGenes,
-            "Face": facialGenes,
             "Lips": lipGenes,
-            "Teeth": teethGenes}
+            "Teeth": teethGenes,
+            "Ears": earGenes,
+            "Arms": armGenes,
+            "Hands": handGenes,
+            "Legs": legGenes,
+            "Feet": footGenes,
+            "Skin Patterns": skinPatternGenes,
+            "Voice": voiceGene}
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import dna_dictionaries as dna
 import dna_generator as gen
 
 
+
 def PrintFeatures(dnaSequence):
 
     encoder = gen.getNumberOfEncodedBases() - 1
@@ -19,15 +20,6 @@ def PrintFeatures(dnaSequence):
 
             read = toRead
 
-    #primary = toRead
-    #read += toRead
-    #toRead = len(list(dna.eye_secondary_colours.keys())[0])
-    #if dna.eye_primary_colours[dnaSequence[read - primary : read]] \
-    #        in dna.eye_secondary_colours[dnaSequence[read : read + toRead]]:
-    #    print("Eyes Secondary Colour: N/A")
-    #else:
-    #    print("Eyes Secondary Colour: " +
-    #          dna.eye_secondary_colours[dnaSequence[read : read + toRead]])
 
 
 def RandomMutation(motherDNA, fatherDNA):
@@ -35,17 +27,23 @@ def RandomMutation(motherDNA, fatherDNA):
     childDNA = ""
 
     for i in range(0,len(motherDNA)):
-        motherGene = motherDNA[i]
-        fatherGene = fatherDNA[i]
+        motherBase = motherDNA[i]
+        fatherBase = fatherDNA[i]
 
-        if random.randint(0,1) == 0:
-            childGene = motherGene
+        randomValue = random.randint(1,100)
+        if randomValue <= 50:
+            childBase = motherBase
         else:
-            childGene = fatherGene
+            childBase = fatherBase
 
-        childDNA = childDNA + childGene
+        if (randomValue == 1) or (randomValue == 100):
+            childBase = TranscriptionError(childBase)
+
+        childDNA = childDNA + childBase
 
     return childDNA
+
+
 
 def SignalSwitching(motherDNA, fatherDNA):
 
@@ -59,16 +57,34 @@ def SignalSwitching(motherDNA, fatherDNA):
         for base in range(gen.getNumberOfEncodedBases()):
             geneLength += gen.bases.index(motherDNA[baseCount + base]) + 1
 
-        if random.randint(0,10) >= 5:
+        if random.randint(1,50) <= 25:
             childGene = motherDNA[baseCount : baseCount + geneLength]
         else:
             childGene = fatherDNA[baseCount : baseCount + geneLength]
+
+        
 
         childDNA = childDNA + childGene
 
         baseCount += geneLength
 
     return childDNA
+
+
+
+def TranscriptionError(dna):
+
+    newDNA = ""
+
+    for base in range(len(dna)):
+        if True:
+            newDNA += gen.bases[random.randint(0,3)]
+        else:
+            newDNA += base
+
+    return newDNA
+
+
 
 #Either "all" or numeric value
 numberOfGenes = "1"
