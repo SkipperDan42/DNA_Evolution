e6f50b007a7edf4b060ae20426c8b177e91edb20
SkipperDan42 <northdan51@gmail.com>
2024-06-14 09:52:00 +0100

    Signal Switching - WORKING
    Signal Switching now works for multiple Chromosomes and prints correctly.

--- main.py
+++ main.py
@@ -11,26 +11,23 @@ def PrintDNASequence(dna):
 def PrintFeatures(chromosomeA, chromosomeB):
 
     encoder = gen.getNumberOfEncodedBases()
-    read = 0
+    basesRead = 0
 
     for geneGroupLabel in gen.genesUsed:
         geneGroup = gen.genesUsed[geneGroupLabel]
 
         #Choose which Chromosome to read
-        chromosomeToRead = ""
-        if chromosomeA[read] == "A" or chromosomeA[read] == "T":
-            chromosomeToRead = chromosomeA
-        else:
-            chromosomeToRead = chromosomeB
+        chromosomeToRead = checkChromosomeToRead(chromosomeA, chromosomeB,
+                                                 basesRead)
 
-        read += encoder
+        basesRead += encoder
 
         for i, gene in enumerate(geneGroup):
-            toRead = len(list(geneGroup[gene].keys())[0])
+            basesToRead = len(list(geneGroup[gene].keys())[0])
             print(gene + ": " +
-                  geneGroup[gene][chromosomeToRead[read : read + toRead]])
+                  geneGroup[gene][chromosomeToRead[basesRead : basesRead + basesToRead]])
 
-            read = toRead
+            basesRead = basesToRead
 
 
 
@@ -66,48 +63,103 @@ def RandomBase(baseA, baseB):
 
     randomValue = random.randint(1, 100)
     if (randomValue == 1) or (randomValue == 100):
-        baseS = gen.bases[random.randint(0, 3)]
+        baseR = gen.bases[random.randint(0, 3)]
     elif randomValue <= 50:
-        baseS = baseA
+        baseR = baseA
     else:
-        baseS = baseB
+        baseR = baseB
 
-    return baseS
+    return baseR
 
 
 
-def SignalSwitching(motherDNA, fatherDNA):
+def SignalSwitching(motherChromosomeA, motherChromosomeB,
+                    fatherChromosomeA, fatherChromosomeB):
 
     numberOfEncodedBases = gen.getNumberOfEncodedBases()
-    childDNA = ""
-    baseCount = 0
+    childChromosomeA = ""
+    childChromosomeB = ""
+    basesRead = 0
 
     for geneGroups in range(len(gen.genesUsed)):
 
+        """
+        Takes the first encoded base to decide which chromosome to read for
+        each parent.
+        """
+        motherChromosomeToRead = checkChromosomeToRead(motherChromosomeA,
+                                                       motherChromosomeB,
+                                                       basesRead)
+        fatherChromosomeToRead = checkChromosomeToRead(fatherChromosomeA,
+                                                       fatherChromosomeB,
+                                                       basesRead)
+
+        """
+        Retrieves the value of the gene length by getting the index values of 
+        the encoded base pairs.
+        """
         geneLength = 0
+        for base in range(1,numberOfEncodedBases):
+            geneLength += gen.bases.index(motherChromosomeA[basesRead + base])
 
-        for base in range(numberOfEncodedBases):
-            geneLength += gen.bases.index(motherDNA[baseCount + base])
+        #Mother Gene on Sex Chromosome
+        motherGeneS = RandomGene(motherChromosomeA[basesRead : basesRead
+                                    + numberOfEncodedBases + geneLength],
+                                 motherChromosomeA[basesRead: basesRead
+                                    + numberOfEncodedBases + geneLength])
+        # Father Sex Gene on Sex Chromosome
+        fatherGeneS = RandomGene(fatherChromosomeA[basesRead: basesRead
+                                    + numberOfEncodedBases + geneLength],
+                                 fatherChromosomeA[basesRead: basesRead
+                                    + numberOfEncodedBases + geneLength])
+        # Child Gene on Chromosome A
+        childGeneA = RandomGene(motherGeneS,fatherGeneS)
+        # Child Gene on Chromosome B
+        childGeneB = RandomGene(motherGeneS,fatherGeneS)
 
-        randomValue = random.randint(1, 100)
-        if randomValue <= 50:
-            childGene = motherDNA[baseCount :
-                        baseCount + geneLength + numberOfEncodedBases]
-        else:
-            childGene = fatherDNA[baseCount :
-                        baseCount + geneLength + numberOfEncodedBases]
+        childChromosomeA = childChromosomeA + childGeneA
+        childChromosomeB = childChromosomeB + childGeneB
 
-        if (randomValue == 1) or (randomValue == 100):
-            randomBase = random.randint(0, geneLength)
-            baseLocation = baseCount + numberOfEncodedBases + randomBase
-            childDNA = childDNA[0 : baseLocation] + \
-                       gen.bases[random.randint(0, 3)] + \
-                       childDNA[baseLocation + 1 : len(childDNA)]
+        basesRead += geneLength + numberOfEncodedBases
 
-        childDNA = childDNA + childGene
-        baseCount += geneLength + numberOfEncodedBases
+    return childChromosomeA, childChromosomeB
 
-    return childDNA
+
+
+def RandomGene(geneA, geneB):
+
+    geneR = ""
+    randomValue = random.randint(1, 100)
+    if randomValue <= 50:
+        geneR = geneA
+    else:
+        geneR = geneB
+
+    """
+    Transcription errors must take place after the gene has been selected.
+    """
+    if (randomValue == 1) or (randomValue == 100):
+        randomBase = random.randint(0, len(geneR))
+        baseLocation = randomBase
+        geneR = geneR[0: baseLocation] + \
+                gen.bases[random.randint(0, 3)] + \
+                geneR[baseLocation + 1: len(geneR)]
+
+    return geneR
+
+
+
+"""
+Decides which chromosome must be read from the encoded base.
+"""
+def checkChromosomeToRead(chromosomeA, chromosomeB, basesRead):
+
+    if chromosomeA[basesRead] == "A" or chromosomeA[basesRead] == "T":
+        chromosomeToRead = chromosomeA
+    else:
+        chromosomeToRead = chromosomeB
+
+    return chromosomeToRead
 
 
 
@@ -115,12 +167,12 @@ def runDNASimulator(numberOfGenes):
 
     motherChromosomeA, motherChromosomeB = gen.BuildSequenceFromGenes(numberOfGenes)
     fatherChromosomeA, fatherChromosomeB = gen.BuildSequenceFromGenes(numberOfGenes)
-    """
+
     childChASignal, childChBSignal = SignalSwitching(motherChromosomeA,
                                                      motherChromosomeB,
                                                      fatherChromosomeA,
                                                      fatherChromosomeB)
-    """
+
     childChAMutation, childChBMutation = RandomDNAMutation(motherChromosomeA,
                                                            motherChromosomeB,
                                                            fatherChromosomeA,
@@ -131,8 +183,8 @@ def runDNASimulator(numberOfGenes):
     print("Father Chromosome A    :" + fatherChromosomeA)
     print("Father Chromosome B    :" + fatherChromosomeB)
 
-    #print("Child Chromosome A [SS]:" + childChASignal)
-    #print("Child Chromosome B [SS]:" + childChBSignal)
+    print("Child Chromosome A [SS]:" + childChASignal)
+    print("Child Chromosome B [SS]:" + childChBSignal)
     print("Child Chromosome A [M] :" + childChAMutation)
     print("Child Chromosome B [M] :" + childChBMutation)
 
@@ -142,8 +194,8 @@ def runDNASimulator(numberOfGenes):
     print("\nFather:")
     PrintFeatures(fatherChromosomeA, fatherChromosomeB)
 
-    #print("\nChild [Signal Switching]:")
-    #PrintFeatures(childChASignal, childChBSignal)
+    print("\nChild [Signal Switching]:")
+    PrintFeatures(childChASignal, childChBSignal)
 
     print("\nChild [Random Mutation]:")
     PrintFeatures(childChAMutation, childChBMutation)
