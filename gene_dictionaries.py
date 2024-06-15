207878e3a53b6a3b457b0cb7c9db36bb8301b9da
SkipperDan42 <northdan51@gmail.com>
2024-06-15 14:23:51 +0100

    Adding Copyright
    Copyright info added to files

--- gene_dictionaries.py
+++ gene_dictionaries.py
@@ -1,3 +1,23 @@
+# Copyright 2024 D North
+# Source: <https://github.com/SkipperDan42/DNA_Evolution>
+#
+# This file is part of DNA_Evolution.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# For a copy of the GNU General Public License see
+# <http://www.gnu.org/licenses/>.
+
+
+
 import dna_dictionaries as dna
 
 
